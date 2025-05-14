d594cb8815f239e5bef6b83e834c5552af855f93
Thiago Santos <t.vaz.santos@gmail.com>
2025-05-14 14:35:46 -0300

    Initial Commit

---
+++ TesteUiKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TesteUiKIT
+//
+//  Created by Thiago Santos on 14/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
